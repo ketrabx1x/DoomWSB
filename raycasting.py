bca44ba1e760db6f8fd77b71e98a75e659e35369
ketrabx1x <ketrabx1x@gmail.com>
2023-03-07 12:26:29 +0100

    first commit

---
+++ raycasting.py
@@ -0,0 +1,13 @@
+import pygame as pg
+import math
+from settings import *
+
+class RayCasting:
+          def __init__(self, game):
+                    self.game = game
+
+          def rey_cast(self):
+                  pass
+
+          def update(self):
+                  self.rey_cast()
