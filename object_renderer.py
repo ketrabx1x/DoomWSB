1b7ce1db7a6c88b8ef56100403aab9e0c5a67baa
ketrabx1x <ketrabx1x@gmail.com>
2023-03-24 16:04:36 +0100

    Edited sky texture

--- object_renderer.py
+++ object_renderer.py
@@ -6,7 +6,7 @@ class ObjectRenderer:
         self.game = game
         self.screen = game.screen
         self.wall_textures = self.load_wall_textures()
-        self.sky_image = self.get_texture('resources/textures/sky.jpg', (WIDTH, HALF_HEIGHT))
+        self.sky_image = self.get_texture('resources/textures/sky.png', (WIDTH, HALF_HEIGHT))
         self.sky_offset = 0
     
     def draw(self):
@@ -37,5 +37,8 @@ class ObjectRenderer:
     #przypisujemy teksture do numerka
     def load_wall_textures(self):
         return{
-            1: self.get_texture('resources/textures/1.png'),            
+            1: self.get_texture('resources/textures/1.png'),
+            2: self.get_texture('resources/textures/2.png'),
+            3: self.get_texture('resources/textures/3.png'),
+            4: self.get_texture('resources/textures/4.png'),
         }
