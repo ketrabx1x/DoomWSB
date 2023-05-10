55c785b110989a97fb65e9384981a031c03e17af
ketrabx1x <ketrabx1x@gmail.com>
2023-05-10 20:54:02 +0200

    Added weapon sound

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from object_renderer import *
 from sprite_object import *
 from object_handler import *
 from weapon import *
+from sound import *
 
 class Game:
           def __init__(self):
@@ -31,6 +32,7 @@ class Game:
                     self.raycasting = RayCasting(self)
                     self.object_handler = ObjectHandler(self)
                     self.weapon = Weapon(self)
+                    self.sound = Sound(self)
 
           #aktualizowanie ekranu
           def update(self):
---
+++ sound.py
@@ -0,0 +1,8 @@
+import pygame as pg
+
+class Sound:
+    def __init__(self, game):
+        self.game = game
+        pg.mixer.init()
+        self.path = 'resources/sounds/'
+        self.shotgun = pg.mixer.Sound(self.path + 'shotgun.mp3')
